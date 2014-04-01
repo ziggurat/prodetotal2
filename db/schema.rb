07c295cfd4661baa060934c6b13724fb834bda50
Manuel Corrales <manuelcorrales@gmail.com>
2014-03-31 21:24:32 -0300

    Modified schema.rb after migrations run

--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,10 @@
 ActiveRecord::Schema.define(version: 20140331184112) do
 
   create_table "competitions", force: true do |t|
-    t.integer  "exact_match_points"
-    t.integer  "result_match_points"
-    t.integer  "no_match_points"
-    t.integer  "no_forecast_points"
+    t.integer  "exact_match_points",  default: 6
+    t.integer  "result_match_points", default: 3
+    t.integer  "no_match_points",     default: 0
+    t.integer  "no_forecast_points",  default: 0
     t.string   "name"
     t.date     "start_date"
     t.date     "end_date"
