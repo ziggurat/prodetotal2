390562d7224e8e81b1b479c735cfc2289036f797
Manuel Corrales <manuelcorrales@gmail.com>
2014-04-03 21:03:25 -0300

    Added validations for Team model and specs

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,4 @@
 class Team < ActiveRecord::Base
+  validates :key, :name, presence: true
+  validates :key, :name, uniqueness: true
 end
--- spec/api/teams_spec.rb
+++ spec/api/teams_spec.rb
@@ -36,7 +36,51 @@ describe "Team management" do
     post "/api/v1/teams", {:name => "Argentina2", :key => "argentina"}
     response.status.should eql(422)  
     post_json = JSON.parse(response.body)
-    pp post_json  
+    expect(post_json.keys.size).to eq(1)
+    expect(post_json.keys.include?("key")).to be true
+  end
+
+  it "doesn't allow to create a Team with same name as existing one" do
+    # Create team
+    post "/api/v1/teams", {:name => "Argentina", :key => "argentina"}
+    response.status.should eql(201)
+
+    post "/api/v1/teams", {:name => "Argentina", :key => "argentina2"}
+    response.status.should eql(422)  
+    post_json = JSON.parse(response.body)
+    expect(post_json.keys.size).to eq(1)
+    expect(post_json.keys.include?("name")).to be true
+  end
+
+  it "doesn't allow to create a Team with same key and name as existing one" do
+    # Create team
+    post "/api/v1/teams", {:name => "Argentina", :key => "argentina"}
+    response.status.should eql(201)
+
+    post "/api/v1/teams", {:name => "Argentina", :key => "argentina"}
+    response.status.should eql(422)  
+    post_json = JSON.parse(response.body)
+    expect(post_json.keys.size).to eq(2)
+    expect(post_json.keys.include?("key")).to be true
+    expect(post_json.keys.include?("name")).to be true    
+  end
+
+  it "doesn't allow to create a Team without key" do
+    # Create team
+    post "/api/v1/teams", {:name => "Argentina"}
+    response.status.should eql(422)  
+    post_json = JSON.parse(response.body)
+    expect(post_json.keys.size).to eq(1)
+    expect(post_json.keys.include?("key")).to be true
+  end
+
+  it "doesn't allow to create a Team without name" do
+    # Create team
+    post "/api/v1/teams", {:key => "argentina"}
+    response.status.should eql(422)  
+    post_json = JSON.parse(response.body)
+    expect(post_json.keys.size).to eq(1)
+    expect(post_json.keys.include?("name")).to be true    
   end
 
 end
