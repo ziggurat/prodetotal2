73b0598bcf842c0f6c11b7f9947c462a60898be5
Manuel Corrales <manuel.corrales@globant.com>
2014-05-16 16:48:00 -0300

    Added methods to check if the user is valid and a method to retrieve the user for the request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,22 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
 
+  def valid_user_token?
+    user_token = request.headers["User-Token"]
+    return false if user_token.nil?
+    # check if user_token exists in the user DB
+    @user = User.from_token(user_token)
+    if @user.nil?
+      false
+    else      
+      true
+    end
+  end
+
+  def request_user
+    @user
+  end
+
   def serve_ember_app
   	render 'layouts/serve_ember_app'
   end
