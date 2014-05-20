28dddf2a1aee055193a3c26937f38efacf647870
Manuel Corrales <manuelcorrales@gmail.com>
2014-05-20 09:44:57 -0300

    Initial draft for login UI

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,6 +1,6 @@
 // For more information see: http://emberjs.com/guides/routing/
 
-Prodetotal2.Router.map(function() {	
+Prodetotal2.Router.map(function() {
 	this.route('login', {path: "/"});
 	this.route('main');
 });
