149f176a65c35bdf9cfee219dcffa3830deae4ca
Manuel Corrales <manuel.corrales@globant.com>
2014-05-23 13:05:10 -0300

    Polished login flow now using store to fetch user

--- app/assets/javascripts/controllers/login_controller.js
+++ app/assets/javascripts/controllers/login_controller.js
@@ -1,34 +1,39 @@
 Prodetotal2.LoginController = Ember.Controller.extend({
-	loginDisabled: true,
-	actions: {
-		login: function() {
-			console.log('Login function in LoginController');
-			FB.login(function(response) {				
-				this.check_fb_login(response);
-			}.bind(this));
-		}
-	},
-	check_fb_login: function(response) {
-		console.log(response);
-		if (response.status === 'connected') {
-			this.userID = response.authResponse.userID;
-			this.userToken = response.authResponse.accessToken;
-			$.ajax({
-				url: "/api/v1/users/" + response.authResponse.userID,
-				beforeSend: function(xhr){
-					xhr.setRequestHeader('Facebook-User-Id', response.authResponse.userID);
-					xhr.setRequestHeader('Facebook-User-Token', response.authResponse.accessToken);
-				}
-			}).done(function(data) {
-				console.log(data);
-				this.transitionToRoute('home');
-			}.bind(this)).fail(function() {
-				this.set('loginDisabled', false);
-			}.bind(this));
-		} else if (response.status === 'not_authorized') {
-			this.set('loginDisabled', false);
-		} else {
-			this.set('loginDisabled', false);
-		}
-	}
+  loginDisabled: true,
+  actions: {
+    login: function() {
+      console.log('Login function in LoginController');
+      FB.login(function(response) {				
+        this.check_fb_login(response);
+      }.bind(this));
+    }
+  },
+  check_fb_login: function(response) {
+    var loginController = this;
+    if (response.status === 'connected') {
+      $.ajaxSetup({
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader('Facebook-User-Id', response.authResponse.userID);
+            xhr.setRequestHeader('Facebook-User-Token', response.authResponse.accessToken);
+        }
+      });
+      this.store.find('user', response.authResponse.userID).then(
+        function(user) {
+          console.log(user);
+          Prodetotal2.currentUser = user;
+          loginController.transitionToRoute('home');
+        },
+        function(error) {
+          loginController.enableLogin();  
+        }
+      );
+    } else if (response.status === 'not_authorized') {
+      loginController.enableLogin();
+    } else {
+      loginController.enableLogin();
+    }
+  },
+  enableLogin: function() {
+    this.set('loginDisabled', false);
+  }
 });
--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -1,16 +1,10 @@
 // http://emberjs.com/guides/models/using-the-store/
 
 Prodetotal2.Store = DS.Store.extend({
-  // Override the default adapter with the `DS.ActiveModelAdapter` which
-  // is built to work nicely with the ActiveModel::Serializers gem.
   adapter: 'DS.RESTAdapter'
 });
 
 DS.RESTAdapter.reopen({
-  namespace: 'api/v1',
-  headers: {
-    "Facebook-User-Id": "secret key",
-    "Facebook-User-Token": "Some header value"
-  }
+  namespace: 'api/v1'
 });
 
