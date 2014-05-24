c72abedbf843d5398a1e354ed734bfbb929d650c
Manuel Corrales <manuelcorrales@gmail.com>
2014-05-23 23:11:50 -0300

    Removed error action as this route inherits that behavior from the authenticated route

--- app/assets/javascripts/routes/home_route.js
+++ app/assets/javascripts/routes/home_route.js
@@ -1,15 +1,5 @@
 Prodetotal2.HomeRoute = Prodetotal2.AuthenticatedRoute.extend({
     model: function() {
       return this.store.findById('user', localStorage.userId);
-    },
-    actions: {
-      error: function(reason, transition) {
-        if (reason.status === 401) {
-          console.log("Unauthorized");
-          this.transitionTo("login");
-        } else {
-          alert('Something went wrong');
-        }
-      }
     }
 });
