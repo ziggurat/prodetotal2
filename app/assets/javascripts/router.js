6865650596ce1e9cecbb6ca3bd5c3db88afe22af
Manuel Corrales <manuel.corrales@globant.com>
2014-05-28 17:21:28 -0300

    Added routes draft

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,6 +1,11 @@
 // For more information see: http://emberjs.com/guides/routing/
 
 Prodetotal2.Router.map(function() {
-	this.resource('login', {path: '/'});
-	this.resource('home');
+  this.resource('login');  
+  this.resource('competitions', function() {
+    this.resource('competition', { path: ':competition_id' });
+    this.resource('games', function() {
+      this.route('forecast');
+    });
+  });
 });
