dc4b7ee1908648fb8e9be2fa9cf9cf6827dbb25d
Manuel Corrales <manuel.corrales@globant.com>
2014-05-28 17:20:41 -0300

    Added logs to Facebook login class

--- app/models/facebook_login.rb
+++ app/models/facebook_login.rb
@@ -8,6 +8,7 @@ class FacebookLogin
     if is_valid_user?
       user = User.where(facebook_id: @fb_user_id).take
       if user.nil?
+        Rails.logger.debug "User not found in the DB"
         user = User.create({facebook_id: @fb_user_id, name: @me['name']})
       end
       user
@@ -21,12 +22,15 @@ class FacebookLogin
   def is_valid_user?
     begin
       @me = @graph.get_object('me')
-      if @me['id'] == @fb_user_id
+      if @me['id'] == @fb_user_id        
         return true
       else
+        Rails.logger.debug "Facebook user id does not match"
         return false
       end
     rescue Koala::Facebook::AuthenticationError => auth_error
+      Rails.logger.debug "Authentication error with facebook"
+      Rails.logger.debug auth_error
       return false
     end
   end
